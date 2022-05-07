c41f4c351120a61d1abea848825db3bd4f4bb034
Ryan Zidago <ryanzidago@gmail.com>
2022-05-07 23:01:04 +0200

    Made file extension configurable

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::fs::File;
 use std::fs;
 use std::io::Write;
 use std::path::Path;
+
 const USAGE: &str = "
 Usage: 
-    reactcomp <component name> <optional target directory>
+    reactcomp <component name> <optional target directory> <optional extension>
 
-Example:
-    reactcomp MyComponent components
+Examples:
+    reactcomp MyComponent
+    reactcomp MyComponent components/ .js
+    reactcomp MyComponent components/ .android.js
 ";
 
 fn main() {
-    let default_dir = ".".to_string();
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -21,18 +23,22 @@ fn main() {
     }
 
     let component_name = args.get(1).unwrap();
+    
+    let default_dir = ".".to_string();
     let target_dir = args.get(2).unwrap_or(&default_dir);
+
+    let default_extension = ".js".to_string();
+    let extension = args.get(3).unwrap_or(&default_extension);
 
     if !Path::new(target_dir).is_dir() {
         fs::create_dir(target_dir).expect("Failed to create directory {target_dir}");
     }
 
     let component: String = create_component_as_string(component_name);
-    let filename = format!("{target_dir}/{component_name}.js");
+    let filename = format!("{target_dir}/{component_name}{extension}");
     let mut file = File::create(&filename).expect("Failed to create file {filename}.js");
 
     write!(file, "{}", component).expect("Failed to write to file {filename}.js");
-
     println!("Created {filename}");
 }
 
